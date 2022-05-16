557a1c04081b9d6cdeb54d99688365d8a12d0b2a
ashrafuloli <rock.ashraful@gmail.com>
2022-05-16 14:47:29 +0600

    filter animation

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { motion } from "framer-motion";
 
-const Movie = ({movie}) => {
+const Movie = ({ movie }) => {
     console.log(movie);
     return (
-        <div>
+        <motion.div
+            layout
+            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0, scale: 0 }}
+            exit={{ opacity: 0, scale: 0 }}
+            transition={{ duration: 0.5 }}
+        >
             <h5>{movie.title}</h5>
             <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt="" />
-        </div>
+        </motion.div>
     );
 };
 
